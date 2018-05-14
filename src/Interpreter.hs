7ee621a97560dd9ca68b0d2cce0739a8a62baed8
Łukasz Raszkiewicz <lraszkiewicz@gmail.com>
2018-05-14 19:39:19 +0200

    execStmt implementation

--- src/Interpreter.hs
+++ src/Interpreter.hs
@@ -1,6 +1,9 @@
+{-# LANGUAGE ParallelListComp #-}
+
 module Interpreter where
 
 import Control.Monad.RWS
+import Data.Maybe (isNothing)
 import Data.Bool
 import Data.Map (Map, (!))
 import qualified Data.Map as Map
@@ -8,13 +11,19 @@ import qualified Data.Map as Map
 import AbsXul
 import ParXul
 
-type Env = Map Ident TopDef
+type FunEnv = Map Ident TopDef
 
 type VarEnv = Map Ident Int
 type Store = Map Int Expr
-type State = (VarEnv, Int, Store)
+-- State: (varEnv, newloc, store, ret)
+--- varEnv - mapping of variables to a place in Store
+--- newloc - next available memory location
+--- store - mapping of memory locations to values
+--- ret - Just $ return value of the currently executed function
+---       or Nothing if not yet returned
+type State = (VarEnv, Int, Store, Maybe Expr)
 
-type ProgMonad = RWST Env () State IO
+type ProgMonad = RWST FunEnv () State IO
 
 interpret :: Program -> IO ()
 interpret (Program topDefs) = print "Hello"
@@ -23,27 +32,108 @@ interpret (Program topDefs) = print "Hello"
   --     (foldr (\def@(FnDef _ ident _ _) -> Map.insert ident def) Map.empty)
   --     ())
 
-emptyEnv :: Env
-emptyEnv = Map.empty
-
 emptyState :: State
-emptyState = (Map.empty, 0, Map.empty)
+emptyState = (Map.empty, 0, Map.empty, Nothing)
 
-initVariable :: (Arg, Expr) -> State -> State
-initVariable (Arg _ ident, expr) (varEnv, newloc, store) =
-  (Map.insert ident newloc varEnv, newloc + 1, Map.insert newloc expr store)
+initVariable :: Item -> State -> State
+initVariable (Init ident expr) (varEnv, newloc, store, ret) =
+  (Map.insert ident newloc varEnv,
+   newloc + 1,
+   Map.insert newloc expr store,
+   ret)
+
+newFunEnv :: FunEnv
+newFunEnv = Map.fromList [
+    (Ident "intToString",
+     FnDef Str (Ident "intToString") [Arg Int (Ident "n")] (Block [])),
+    (Ident "stringToInt",
+     FnDef Int (Ident "stringToInt") [Arg Str (Ident "s")] (Block []))
+  ]
 
 execFun :: TopDef -> [Expr] -> ProgMonad Expr
+execFun (FnDef _ (Ident "intToString") _ _) [exprArg] = do
+  evalArg <- eval exprArg
+  case evalArg of
+    ELitInt n -> return $ EString $ show n
+execFun (FnDef _ (Ident "stringToInt") _ _) [exprArg] = do
+  evalArg <- eval exprArg
+  case evalArg of
+    EString s -> return $ ELitInt $ read s
 execFun (FnDef _ _ args block) exprArgs = do
+  -- TODO
   evalArgs <- forM exprArgs eval
   oldState <- get
-  put $ foldr initVariable emptyState $ zip args evalArgs
+  let inits = [Init ident expr | (Arg _ ident) <- args | expr <- evalArgs]
+  put $ foldr initVariable emptyState inits
   return ELitTrue
+
+valToString :: Expr -> String
+valToString val = case val of
+  ELitInt n -> show n
+  ELitTrue -> "True"
+  ELitFalse -> "False"
+  EString s -> s
+
+execStmt :: Stmt -> ProgMonad ()
+execStmt stmt = do
+  oldState@(varEnv, newloc, store, ret) <- get
+  when (isNothing ret) $ case stmt of
+    Empty -> return ()
+    BStmt (Block stmts) -> do
+      forM_ stmts execStmt
+      (_, newNewloc, newStore, newRet) <- get
+      put (varEnv, newNewloc, newStore, newRet)
+    Decl _ vars -> do
+      vals <- forM [expr | (Init _ expr) <- vars] eval
+      let inits = [Init ident val | (Init ident _) <- vars | val <- vals]
+      put $ foldr initVariable oldState inits
+    Ass ident expr -> do
+      val <- eval expr
+      let newStore = Map.insert (varEnv ! ident) val store
+      put (varEnv, newloc, newStore, ret)
+    Incr ident -> do
+      let (ELitInt val) = store ! (varEnv ! ident)
+      let newStore = Map.insert (varEnv ! ident) (ELitInt $ val + 1) store
+      put (varEnv, newloc, store, ret)
+    Decr ident -> do
+      let (ELitInt val) = store ! (varEnv ! ident)
+      let newStore = Map.insert (varEnv ! ident) (ELitInt $ val - 1) store
+      put (varEnv, newloc, store, ret)
+    Ret expr -> do
+      val <- eval expr
+      put (varEnv, newloc, store, Just val)
+    -- The return value from VRet will be ignored, it just can't be Nothing.
+    VRet -> put (varEnv, newloc, store, Just ELitTrue)
+    Cond expr body -> do
+      val <- eval expr
+      when (val == ELitTrue) $ execStmt body
+    CondElse expr bodyIf bodyElse -> do
+      val <- eval expr
+      execStmt (if val == ELitTrue then bodyIf else bodyElse)
+    loop@(While expr body) -> do
+      val <- eval expr
+      when (val == ELitTrue) $ do
+        execStmt body
+        execStmt loop
+    SExp _ -> return ()
+    Print exprs -> do
+      vals <- forM exprs eval
+      lift $ putStr $ unwords $ map valToString vals
+      lift $ putStr "\n"
+    For t ident exprStart ord exprEnd body -> do
+      ELitInt valStart <- eval exprStart
+      ELitInt valEnd <- eval exprEnd
+      let compOp =
+            if ord == OrdUp then (<=) else (>=) :: Integer -> Integer -> Bool
+      let iterOp = if ord == OrdUp then (+) else (-)
+      when (compOp valStart valEnd) $ do
+        execStmt body
+        execStmt $ For t ident (ELitInt $ iterOp valStart 1) ord exprEnd body
 
 eval :: Expr -> ProgMonad Expr
 eval e = case e of
   EVar ident -> do
-    (varEnv, _, store) <- get
+    (varEnv, _, store, _) <- get
     return $ store ! (varEnv ! ident)
   EApp ident args -> do
     funEnv <- ask
@@ -77,13 +167,13 @@ eval e = case e of
   ERel e1 op e2 -> do
     e3 <- eval e1
     e4 <- eval e2
-    op2 <- case op of
-      LTH -> return (<)
-      LE -> return (<=)
-      GTH -> return (>)
-      GE -> return (>=)
-      EQU -> return (==)
-      NE -> return (/=)
+    let op2 = case op of
+          LTH -> (<)
+          LE -> (<=)
+          GTH -> (>)
+          GE -> (>=)
+          EQU -> (==)
+          NE -> (/=)
     let res = op2 e3 e4
     return $ if res then ELitTrue else ELitFalse
   EAnd e1 e2 -> do
